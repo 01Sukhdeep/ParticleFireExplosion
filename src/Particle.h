aa2308924dadc405dc44d94ac94fa9dc97b02d84
Sukhdeep <shaktimaan9431@gmail.com>
2022-01-13 17:55:13 +0530

    Other files added

---
+++ src/Particle.h
@@ -0,0 +1,18 @@
+namespace caveofprogramming{
+    struct Particle{
+        double m_x;
+        double m_y;
+
+    private:
+        double m_speed;
+        double m_direction;
+
+    private:
+        void init();
+
+    public:
+        Particle();
+        virtual ~Particle();
+        void update(int interval);
+    };
+}
